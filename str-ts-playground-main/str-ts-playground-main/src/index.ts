58c1a46361903d63b2ab43a4e049256b47b31dde
Szabo Laszlo <szabo.laszlo@szawary.eu>
2021-01-21 05:59:54 +0100

    class készítés menete

--- str-ts-playground-main/str-ts-playground-main/src/index.ts
+++ str-ts-playground-main/str-ts-playground-main/src/index.ts
@@ -1,3 +1,36 @@
 import './style.scss';
 
-console.log(`Program has been inited.`);
+//const user = {
+//    name: 'Juli',
+//    e- mail: 'juli@gmail.com',
+//        hello() {
+//    return `Hello, I ma ${this.name}.`;
+//}
+//};
+//const user2 = {
+//    name: 'Juli',
+//    e- mail: 'juli@gmail.com',
+//        hello() {
+//    return `Hello, I ma ${this.name}.`;
+//}
+//};
+//console.log(user.name);
+//console.log(user.name2);
+
+
+// osztályt használunk az előbbi helyett.
+class User {
+    name: string = '';
+    email: string = '';
+    age: number = 18;
+    hello(): string {
+        return `Szia, a nevem ${this.name}`;
+    }
+}; // ez az alapja az új objektumnak
+
+const user1 = new User();
+const user2 = new User();
+user2.name = 'Jenő';
+const user3 = new User();
+
+console.log(user2.hello());

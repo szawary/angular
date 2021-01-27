c045d9168fa52a8bf82daae944da469093d7264e
Szabo Laszlo <szabo.laszlo@szawary.eu>
2021-01-27 20:53:28 +0100

    ang-basic-practice002 házifeladat kész

--- str-angular-001/ang-basic-practice002-data-binding/src/app/app.component.ts
+++ str-angular-001/ang-basic-practice002-data-binding/src/app/app.component.ts
@@ -8,11 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Struktúraváltás';
 
-  menuItems: {title: string, link: string, order: number}[] = [
-    {title: 'Támogatás', link: '/support', order: 3},
-    {title: 'Vállalati ügyfelek', link: '/enterprise', order: 2},
-    {title: 'Szolgáltatások', link: '/services', order: 1},
-    {title: 'Árak', link: '/prices', order: 4},
+  menuItems: { title: string, link: string, order: number }[] = [
+    { title: 'Támogatás', link: '/support', order: 3 },
+    { title: 'Vállalati ügyfelek', link: '/enterprise', order: 2 },
+    { title: 'Szolgáltatások', link: '/services', order: 1 },
+    { title: 'Árak', link: '/prices', order: 4 },
   ];
 
   regButtonText: string = 'Regisztráció';
--- str-angular-001/ang-basic-practice002-data-binding/src/app/pricing-page/pricing-page.component.ts
+++ str-angular-001/ang-basic-practice002-data-binding/src/app/pricing-page/pricing-page.component.ts
@@ -14,7 +14,11 @@ interface IPriceItem {
 })
 export class PricingPageComponent implements OnInit {
 
-  priceTable: IPriceItem[] = [];
+  priceTable: IPriceItem[] = [
+    { name: 'Free', price: 0, options: ['10 tanuló', '2 GB tárhely', 'Email támogatás', 'Dokumentáció'], btnText: 'Ingyenes regisztráció' },
+    { name: 'Pro', price: 15, options: ['20 tanuló', '10 GB tárhely', 'Kiemelt email támogatás', 'Dokumentáció'], btnText: 'Kezdő lépések' },
+    { name: 'Enterprise', price: 29, options: ['100 tanuló', '100 GB tárhely', 'Telefonos és email támogatás', 'Dokumentáció'], btnText: 'Kontakt' },
+  ];
 
   constructor() { }
 
